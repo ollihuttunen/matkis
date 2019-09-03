c582af2512e94a15e1fd37c0e11bb26ed0d17ffb
Olli Huttunen <Olli@Olli-MacBook-Pro-995.local>
2019-09-03 13:46:13 +0300

    latasin css prefixerin ja asetin pipe filtterit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
 
 gulp.task ('default', function(){
 	console.log ("hip hei! - jäbä teki gulpit");
@@ -10,7 +15,12 @@ gulp.task ('html', function(){
 });
 
 gulp.task ('styles', function(){
-	console.log ("jäbä teki nyt muutoksia css tyyleihin");
+	
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+	
+	
 });
 
 
